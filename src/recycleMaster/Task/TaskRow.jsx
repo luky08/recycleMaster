cf9ef3930d04768ede6b38745dd4f01efe9ae48e
Lukáš Helcl <luky.helcl@gmail.com>
2025-09-01 18:15:06 +0200

    feat: add TaskRow

---
+++ src/recycleMaster/Task/TaskRow.jsx
@@ -0,0 +1,34 @@
+import './TaskRow.css'
+function TaskRow({imgUrl, lvl, taskName, weight, time, price}) {
+    return(
+        <div className="main-row">
+          <div
+            className="left"
+          >
+            <img src={imgUrl} alt="Převoz odpadu" />
+          </div>
+          <div className="middle">
+            <div className="text">
+              <div>
+                {lvl} - {taskName}
+              </div>
+              <div>
+                {weight} kg <span> {time} min </span>
+              </div>
+            </div>
+            <div className="progress-bar">
+              
+                <div
+                  className="progress"
+                ></div>
+            </div>
+          </div>
+          <div className="right" >
+            <img src="/assets/upgrade-text.png" alt="Upgrade" />
+            <div className="price"> {price} kč</div>
+          </div>
+        </div>
+    )
+}
+
+export default TaskRow;
