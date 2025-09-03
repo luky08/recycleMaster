ab6166ce195108583d07652f8933f5641ac24374
Lukáš Helcl <luky.helcl@gmail.com>
2025-09-03 10:57:22 +0200

    feat: add reStoreRow

---
+++ src/recycleMaster/ReStore/ReStoreRow.jsx
@@ -0,0 +1,24 @@
+import './ReStoreRow.css';
+
+
+function ReStoreRow({imgUrl, imgAlt, name, quantity, price}){
+    return(
+        <div className="main-row store">
+          <div className="left">
+            <img src={imgUrl} alt={imgAlt} />
+          </div>
+          <div className="middle">
+            <div className="text">
+              <div>{name}</div>
+              <div>Quantity: {quantity} <span> Price: {price} kč</span></div>
+            </div>
+          </div>
+          <div className="right">
+            <button className="sell-one">Sell 1x</button>
+            <button className="sell-all">Sell all</button>
+          </div>
+        </div>
+    )
+}
+
+export default ReStoreRow;
