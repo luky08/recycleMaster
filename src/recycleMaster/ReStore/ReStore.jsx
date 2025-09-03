d1fcb9b3220a66bf99d67c74cebd00e68e08dcef
Lukáš Helcl <luky.helcl@gmail.com>
2025-09-03 11:15:01 +0200

    feat: add reStoreRow to reStore

--- src/recycleMaster/ReStore/ReStore.jsx
+++ src/recycleMaster/ReStore/ReStore.jsx
@@ -1,6 +1,13 @@
+import ReStoreRow from './ReStoreRow'
+
 function ReStore(){
     return(
-        "ReStore"
+        <>
+            <ReStoreRow imgUrl="assets/t-shirt.png" imgAlt="Img t-shirt" name="T-shirt" quantity={2} price={8}/>
+            <ReStoreRow imgUrl="assets/bottle.png" imgAlt="Img bottle" name="Bottle" quantity={5} price={12}/>
+            <ReStoreRow imgUrl="assets/notebook.png" imgAlt="Img notebook" name="Notebook" quantity={12} price={15}/>
+            <ReStoreRow imgUrl="assets/window.png" imgAlt="Img window" name="Window" quantity={0} price={85}/>
+        </>
     )
 }
 
