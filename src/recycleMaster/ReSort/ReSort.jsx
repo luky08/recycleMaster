4815fe5d55bcfe3cb899d703bb73bbf09451fc84
Lukáš Helcl <luky.helcl@gmail.com>
2025-09-05 10:11:07 +0200

    feat: add button for buy waste

--- src/recycleMaster/ReSort/ReSort.jsx
+++ src/recycleMaster/ReSort/ReSort.jsx
@@ -1,11 +1,20 @@
 import './ReSort.css'
 import TaskRow from '../Task/TaskRow'
+import WasteStats from '../WasteStats/WasteStats';
+import BtnAddWaste from '../WasteStats/BtnAddWaste';
 
 function ReSort() {
     return(
         <>
-        <TaskRow widthProgress={15} imgUrl="/assets/ReCar.png" imgAlt="Img ReCar" lvl={1} taskName="Transport of waste to the sorting facility" weight1={8} time={5} price={25}/>
-        <TaskRow widthProgress={15} imgUrl="/assets/ReSorting.png" imgAlt="Img reSort" lvl={1} taskName="Waste sorting into plastic, glass, and paper" weight1={8} time={18} price={25}/>
+        <div className="right-section">
+            <TaskRow widthProgress={15} imgUrl="/assets/ReCar.png" imgAlt="Img ReCar" lvl={1} taskName="Transport of waste to the sorting facility" weight1={8} time={5} price={25}/>
+            <div className="ready-sorting">Waste ready for sorting: 10</div>
+            <TaskRow widthProgress={15} imgUrl="/assets/ReSorting.png" imgAlt="Img reSort" lvl={1} taskName="Waste sorting into plastic, glass, and paper" weight1={8} time={18} price={25}/>
+            <div className="btn-add-waste">
+                <BtnAddWaste/>
+            </div>
+        </div>
+
         </>
     )
 }
--- src/recycleMaster/WasteStats/BtnAddWaste.jsx
+++ src/recycleMaster/WasteStats/BtnAddWaste.jsx
@@ -5,3 +5,5 @@ function BtnAddWaste(){
         </button>
     )
 }
+
+export default BtnAddWaste;
