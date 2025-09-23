619818ddf970dad6034a1319309ad6bf3776e1e5
Lukáš Helcl <luky.helcl@gmail.com>
2025-09-23 17:30:50 +0200

    refactor: add map for store

--- src/recycleMaster/ReStore/ReStore.jsx
+++ src/recycleMaster/ReStore/ReStore.jsx
@@ -9,13 +9,23 @@ function ReStore(){
     const notebook = useSelector((s) => s.game.notebook)
     const window = useSelector((s) => s.game.window)
 
+
+
+
+    const products = [
+        { id: 1, name:"shirt", imgUrl: "assets/t-shirt.png", imgAlt:"Img t-shirt", nameProduct:"T-shirt", quantity: tshirt, price: 8 },
+        { id: 2, name:"bottle", imgUrl: "assets/bottle.png", imgAlt:"Img bottle", nameProduct:"Bottle", quantity: bottle, price: 12 },
+        { id: 3, name:"notebook", imgUrl: "assets/notebook.png", imgAlt:"Img notebook", nameProduct:"Notebook", quantity: notebook, price: 15 },
+        { id: 4, name:"window", imgUrl: "assets/window.png", imgAlt:"Img window", nameProduct:"T-shirt", quantity: window, price: 85 },
+    ];
+
     return(
         <>
-        
-            <ReStoreRow imgUrl="assets/t-shirt.png" imgAlt="Img t-shirt" name="T-shirt" quantity={tshirt} price={8}/>
-            <ReStoreRow imgUrl="assets/bottle.png" imgAlt="Img bottle" name="Bottle" quantity={bottle} price={12}/>
-            <ReStoreRow imgUrl="assets/notebook.png" imgAlt="Img notebook" name="Notebook" quantity={notebook} price={15}/>
-            <ReStoreRow imgUrl="assets/window.png" imgAlt="Img window" name="Window" quantity={window} price={85}/>
+        {products.map((p) => (
+            
+            <ReStoreRow key={p.id} imgUrl={p.imgUrl} imgAlt={p.imgAlt} nameProduct={p.nameProduct} quantity={p.quantity} price={p.price}/>
+        ))}
+            
         </>
     )
 }
