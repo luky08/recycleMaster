d2c0e8456d5749252c37baa17593e1c55c37312b
Lukáš Helcl <luky.helcl@gmail.com>
2025-09-24 11:47:52 +0200

    add: json file and maping to reCraft and reStore

--- src/recycleMaster/ReCraft/ReCraft.jsx
+++ src/recycleMaster/ReCraft/ReCraft.jsx
@@ -1,6 +1,7 @@
 import TaskRow from "../Task/TaskRow";
 import { useSelector, useDispatch } from "react-redux";
 import {craft, finishCrafting} from '../State/gameSlice';
+import products from '../../products.json';
 
 function ReCraft(){
     
@@ -9,70 +10,28 @@ function ReCraft(){
     const alert = useSelector((s) => s.game.alert)
     return(
         <>
-            <TaskRow
-                name="tshirt"
-                widthProgress={0} 
-                imgUrl="/assets/t-shirt.png" 
-                imgAlt="Img T-shirt" 
-                lvl={1} 
-                taskName="Make a T-shirt" 
-                time={8} 
-                price={25} 
-                weight1={4} 
-                trashName1="plastic"
-                alert={alert}
-                onActivate={(name, trashName1, weight1, trashName2, weight2) => dispatch(craft({name, trashName1, weight1, trashName2, weight2}))}
-                onFinish={(name, trashName1, weight1, trashName2, weight2) => dispatch(finishCrafting({name, trashName1, weight1, trashName2, weight2}))}
+        
+            {products.map((p) => (
+                <TaskRow
+                    key={p.id}
+                    name={p.name}
+                    widthProgress={0} 
+                    imgUrl={`assets/${p.imgName}`} 
+                    imgAlt={p.imgAlt}
+                    taskName={p.taskName}
+                    trashNameFirst={p.trashNameFirst}
+                    trashNameSecond={p.trashNameSecond}
+                    lvl={1}  
+                    time={8} 
+                    price={p.levels.upPrice} 
+                    weightFirst={p.weightFirst} 
+                    weightSecond={p.weightSecond7}
+                    alert={alert}
+                    onActivate={(name, trashNameFirst, weightFirst, trashNameSecond, weightSecond) => dispatch(craft({name, trashNameFirst, weightFirst, trashNameSecond, weightSecond}))}
+                    onFinish={(name, trashNameFirst, weightFirst, trashNameSecond, weightSecond) => dispatch(finishCrafting({name, trashNameFirst, weightFirst, trashNameSecond, weightSecond}))}
 
-            />
-            <TaskRow 
-                name="bottle"
-                widthProgress={0} 
-                imgUrl="/assets/bottle.png" 
-                imgAlt="Img Bottle" 
-                lvl={1} 
-                taskName="Make a Bottle"
-                time={12} 
-                price={25} 
-                weight1={6} 
-                trashName1="glass"
-                alert={alert}
-                onActivate={(name, trashName1, weight1, trashName2, weight2) => dispatch(craft({name, trashName1, weight1, trashName2, weight2}))}
-                onFinish={(name, trashName1, weight1, trashName2, weight2) => dispatch(finishCrafting({name, trashName1, weight1, trashName2, weight2}))}
-
-           />
-            <TaskRow 
-                name="notebook"
-                widthProgress={0} 
-                imgUrl="/assets/notebook.png" 
-                imgAlt="Img NoteBook" 
-                lvl={1} 
-                taskName="Make a NoteBook" 
-                time={15} 
-                price={25} 
-                weight1={4} 
-                trashName1="paper"
-                alert={alert}
-                onActivate={(name, trashName1, weight1, trashName2, weight2) => dispatch(craft({name, trashName1, weight1, trashName2, weight2}))}
-                           onFinish={(name, trashName1, weight1, trashName2, weight2) => dispatch(finishCrafting({name, trashName1, weight1, trashName2, weight2}))}
-
-           />
-            <TaskRow 
-                name="window"
-                widthProgress={0} 
-                imgUrl="/assets/window.png" 
-                imgAlt="Img window" 
-                lvl={1} 
-                taskName="Make a Window" 
-                time={35} 
-                price={25} 
-                weight1={5} 
-                trashName1="plastic" 
-                weight2={8} 
-                trashName2="glass"
-                onFinish={(name, trashName1, weight1, trashName2, weight2) => dispatch(finishCrafting({name, trashName1, weight1, trashName2, weight2}))}
-                onActivate={(name, trashName1, weight1, trashName2, weight2) => dispatch(craft({name, trashName1, weight1, trashName2, weight2}))}
-            />
+                />
+            ))}
         </>
     )
 }
--- src/recycleMaster/ReStore/ReStore.jsx
+++ src/recycleMaster/ReStore/ReStore.jsx
@@ -1,29 +1,19 @@
 import ReStoreRow from './ReStoreRow'
 import { useSelector } from 'react-redux';
+import products from '../../products.json'
+
 
 
 
 function ReStore(){
-    const tshirt = useSelector((s) => s.game.tshirt)
-    const bottle = useSelector((s) => s.game.bottle)
-    const notebook = useSelector((s) => s.game.notebook)
-    const window = useSelector((s) => s.game.window)
 
 
-
-
-    const products = [
-        { id: 1, name:"shirt", imgUrl: "assets/t-shirt.png", imgAlt:"Img t-shirt", nameProduct:"T-shirt", quantity: tshirt, price: 8 },
-        { id: 2, name:"bottle", imgUrl: "assets/bottle.png", imgAlt:"Img bottle", nameProduct:"Bottle", quantity: bottle, price: 12 },
-        { id: 3, name:"notebook", imgUrl: "assets/notebook.png", imgAlt:"Img notebook", nameProduct:"Notebook", quantity: notebook, price: 15 },
-        { id: 4, name:"window", imgUrl: "assets/window.png", imgAlt:"Img window", nameProduct:"T-shirt", quantity: window, price: 85 },
-    ];
 
     return(
         <>
         {products.map((p) => (
             
-            <ReStoreRow key={p.id} imgUrl={p.imgUrl} imgAlt={p.imgAlt} nameProduct={p.nameProduct} quantity={p.quantity} price={p.price}/>
+            <ReStoreRow key={p.id} imgUrl={`assets/${p.imgName}`} imgAlt={p.imgAlt} nameProduct={p.nameProduct} quantityName={p.name} price={p.price}/>
         ))}
             
         </>
